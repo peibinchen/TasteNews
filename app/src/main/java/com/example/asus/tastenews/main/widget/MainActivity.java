7a9231b84a06f256f8e525609d13d83982a73bd4
cpb <1198960858@qq.com>
2016-09-15 20:29:33 +0800

    change the theme of tabLayout and add toolbars to comment areas

--- app/src/main/java/com/example/asus/tastenews/main/widget/MainActivity.java
+++ app/src/main/java/com/example/asus/tastenews/main/widget/MainActivity.java
@@ -6,7 +6,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -28,7 +27,6 @@ import com.iflytek.cloud.SpeechUtility;
 import com.iflytek.sunflower.FlowerCollector;
 
 import java.lang.reflect.Method;
-import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity implements MainView,SpeechRecognitionUtils.SpeechRecognitionCallback {
 
@@ -272,9 +270,5 @@ public class MainActivity extends AppCompatActivity implements MainView,SpeechRe
         FlowerCollector.onPageEnd(TAG);
         FlowerCollector.onPause(this);
         super.onPause();
-    }
-
-    public interface OnThemeSwitchListener{
-        void switch2Theme(HashMap<String,TypedValue> themes);
     }
 }
